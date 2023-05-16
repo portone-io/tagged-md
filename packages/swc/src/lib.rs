d6f608ee354271d918e1b9f89a2990b00d455df3
Iha Shin <me@xiniha.dev>
2023-05-16 14:14:41 +0900

    feat: initialize with initial swc plugin impl

---
+++ packages/swc/src/lib.rs
@@ -0,0 +1,54 @@
+use swc_core::plugin::{plugin_transform, proxies::TransformPluginProgramMetadata};
+use swc_core::{
+    common::DUMMY_SP,
+    ecma::{
+        ast::{Expr, Lit, Program, Str},
+        transforms::testing::test,
+        visit::{as_folder, FoldWith, VisitMut, VisitMutWith},
+    },
+};
+
+pub struct TransformVisitor;
+
+impl VisitMut for TransformVisitor {
+    fn visit_mut_expr(&mut self, expr: &mut Expr) {
+        expr.visit_mut_children_with(self);
+
+        if let Expr::TaggedTpl(tpl) = expr {
+            if let Expr::Ident(ident) = tpl.tag.as_mut() {
+                if ident.sym.eq("md") {
+                    if tpl.tpl.quasis.len() != 1 {
+                        panic!("md`` template literal shouldn't have any expressions inside");
+                    }
+                    *expr = Lit::Str(Str {
+                        span: DUMMY_SP,
+                        value: markdown::to_html(&tpl.tpl.quasis.first().unwrap().raw.to_string()).into(),
+                        raw: None,
+                    })
+                    .into()
+                }
+            }
+        }
+    }
+}
+
+#[plugin_transform]
+pub fn process_transform(program: Program, _metadata: TransformPluginProgramMetadata) -> Program {
+    program.fold_with(&mut as_folder(TransformVisitor))
+}
+
+test!(
+    Default::default(),
+    |_| as_folder(TransformVisitor),
+    processes_markdown_paragraph,
+    r#"console.log(md`foo`);"#,
+    r#"console.log("<p>foo</p>");"#
+);
+
+test!(
+    Default::default(),
+    |_| as_folder(TransformVisitor),
+    processes_markdown_bold,
+    r#"console.log(md`**foo**`);"#,
+    r#"console.log("<p><strong>foo</strong></p>");"#
+);
